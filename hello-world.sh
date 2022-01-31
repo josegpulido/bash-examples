c176464beebc3064906b1855bea87f24b568104f
josegpulido <memo.board.9999@gmail.com>
2022-01-31 12:57:31 -0600

    feat: Hola mundo con declaración y exportación de variables

--- hello-world.sh
+++ hello-world.sh
@@ -10,3 +10,18 @@
 
 # En el caso de bash, no se escribe ; al final de una sentencia
 echo "Hola, mundo"
+
+# La declaración de variables en bash se hace de modo implícito, es decir,
+# no es necesario especificar de qué tipo de variable se trata. La convención
+# es utilizar nombres en mayúsculas.
+#
+# El scope de las variables creadas se limita solo a la ejecución del
+# programa. En caso de que se desee exportar explicitamente una variable
+# con la palabra reservada export, dicha variable estará disponible en cualquier
+# otro script que se ejecute desde este script, como si fuese una cadena de
+# ejecución.
+export NOMBRE="Jose Guillermo"
+ESTATURA=1.76
+
+echo "Soy $NOMBRE y mido $ESTATURA"
+echo "La variable NOMBRE está siendo exportada"
